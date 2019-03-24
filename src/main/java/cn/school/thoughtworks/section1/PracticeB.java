a3743fcf21a5ecfc9bb6d123d9f0a40e5d75a54e
facccge <fcfc5702@qq.com>
2019-03-24 17:43:53 +0800

    completed Section1 PracticeB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List result = new ArrayList();
+        for(String string1:collection1){
+            for(List<String> childList:collection2){
+                for(String string2:childList){
+                    if (string1.equals(string2)) {
+                        result.add(string1);
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
